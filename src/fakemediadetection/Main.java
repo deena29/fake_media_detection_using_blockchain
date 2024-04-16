b78286b4bf518bbe38df3e8fb45b4f26c0abebbe
deena29 <deena.k@nandhatech.org>
2024-04-16 10:30:49 +0530

    fake news

---
+++ src/fakemediadetection/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fakemediadetection;
+
+/**
+ *
+ * @author Elcot
+ */
+public class Main {
+    public static void main(String[] args) throws Exception
+    {                          
+        MainFrame cf=new MainFrame();
+        cf.setTitle("Main Frame");
+        cf.setVisible(true);
+        cf.setResizable(false);	        
+    }
+}
+
